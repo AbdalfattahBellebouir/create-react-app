7ca79654e7a48cb83302444819e5193fa97c869d
Abdalfattah BELLEBOUIR <73496796+AbdalfattahBellebouir@users.noreply.github.com>
2020-12-03 16:27:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Tweet from './Tweet';
 
 function App(){
 
+  const [users , setUsers] = useState([
+    {name: "Ahmed", message: "Software Engineer", nlikes: "5000"},
+    {name: "Hind", message: "HR Specialist", nlikes: "4000"},
+    {name: "Mehdi", message: "Cybersecurity Expert", nlikes: "8565"},
+    {name: "Sara", message: "DevOps Engineer", nlikes: "6000"}
+  ]);
+
   return(
     <div className='app'>
-      <Tweet name="Ahmed" message="Software Engineer" nlikes = "5000"/>
-      <Tweet name="Hind" message="HR Specialist" nlikes="4000"/>
-      <Tweet name="Mehdi" message="Cybersecurity Expert" nlikes="8565"/>
-      <Tweet name="Sara" message="DevOps Engineer" nlikes="6000"/>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} nlikes={user.nlikes}/>
+      ))}
     </div>
   );
 }
